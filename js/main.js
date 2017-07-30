8d90d89ea79b280b8f90be5edd504a42dad1fec2
njey123 <njey123@users.noreply.github.com>
2017-07-30 09:49:12 -0400

    Parse data from webpage, create schedule layout

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 function main() {
+  // Boolean used to determine if description for course list is already on the page
+  var isDescOnPage = false;
+  // Boolean used to determine if invalid URL message is already on the page
+  var isInvalidURLMessageOnPage = false;
+  // Boolean used to determine if schedule is already on the page
+  var isScheduleOnPage = false;
+  // Contains data from Schedule of Classes URL
+  var data = [];
+  
   // Schedule of Classes URL
   document.getElementById('scheduleOfClassesLinkInput').onkeypress = function(e){
     if (!e) e = window.event;
@@ -7,51 +16,62 @@ function main() {
     var scheduleOfClassesSubstr = scheduleOfClassesURL.substring(0, 26);
 
     // If Enter pressed but input is empty or not from the Schedule of Classes website
-    if ((keyCode == '13' && scheduleOfClassesURL.length === 0) || scheduleOfClassesSubstr !== "https://info.uwaterloo.ca/") {
-      alert('Error: Invalid URL entered.');
+    if (keyCode == '13' && (scheduleOfClassesURL.length === 0 || scheduleOfClassesSubstr !== "https://info.uwaterloo.ca/")) {
+      addInvalidURLMessage();
       return false;
 
-    // Else if Enter is pressed
+    // Else if Enter is pressed, get info from URL
     } else if (keyCode == '13') {
-      var data = { 
+      
+      var inputData = { 
         scheduleOfClassesURL: scheduleOfClassesURL
       };
 
-      var promise = $.getJSON("php/getURLInfo.php", data);
+      var promise = $.getJSON("php/getURLInfo.php", inputData);
       
       promise.done(function(response) { 
-        console.log(response); 
+        data = response;
       });
+
+      removeInvalidURLMessage();
+      createScheduleLayout();
 
       return false;
     }
   }
-  
-  // Boolean used to determine if description for course list is already on the page
-  var isDescOnPage = false;
 
   // Add a course when user presses Enter
   document.getElementById('courseItemInput').onkeypress = function(e){
     if (!e) e = window.event;
     var keyCode = e.keyCode || e.which;
-    var courseToAdd = ($('input[name=courseItem]').val()).trim();
+    var scheduleOfClassesURL = ($("input[name=scheduleOfClassesLink]").val()).trim();
+    var scheduleOfClassesSubstr = scheduleOfClassesURL.substring(0, 26);
 
-    // If Enter pressed but course input is empty
-    if (keyCode == '13' && courseToAdd.length === 0) {
-      return false;
-    // Else if Enter is pressed
-    } else if (keyCode == '13') {
-      $(".courseList").append('<div class="item">' + "• " + courseToAdd + '</div>');
-
-      var element = document.getElementById("courseListID");
-      var numberOfChildren = element.getElementsByTagName('*').length;
-
-      if (numberOfChildren === 1 && !isDescOnPage) {
-        // Display small description that is placed above list of courses
-        $(".courseList").prepend('<p class="descForCourseList">Displaying course timings for: </p>');
-        isDescOnPage = true;
+    // If Enter key is pressed and valid URL is entered
+    if (keyCode == '13' && scheduleOfClassesURL.length !== 0 && scheduleOfClassesSubstr === "https://info.uwaterloo.ca/") {
+      var courseToAdd = ($('input[name=courseItem]').val()).trim();
+  
+      // If course input is empty
+      if (courseToAdd.length === 0) {
+        return false;
+      } else {
+        for(var i = 0; i < data.length; i++) {
+          if (courseToAdd === data[i]["courseCode"]) {
+            $(".courseList").append('<div class="item">' + "• " + courseToAdd + '</div>');
+  
+            var element = document.getElementById("courseListID");
+            var numberOfChildren = element.getElementsByTagName('*').length;
+      
+            if (numberOfChildren === 1 && !isDescOnPage) {
+              // Display small description that is placed above list of courses
+              $(".courseList").prepend('<p class="descForCourseList">Displaying course timings for the following courses (click a course to remove it): </p>');
+              isDescOnPage = true;
+            }
+          }
+        }
+        return false;
       }
-
+    } else if(keyCode == '13') {
       return false;
     }
   }
@@ -69,6 +89,70 @@ function main() {
       isDescOnPage = false;
     }
   });
+
+  // // Parse HTML from Schedule of Classes webpage
+  // function parseHTML(response) {
+  //   var lines = response.split('\n');
+  //   var courseCodeLines = [];
+  //   var courseCodeIdx = 0;
+// 
+  //   for (var line = 0; line < lines.length; line++){
+  //     // Find course codes
+  //     if (lines[line] === '<TR><TD ALIGN="center">BME     </TD>') {
+  //       // console.log(lines[line + 1]);
+  //       courseCodeLines[courseCodeIdx] = lines[line];
+  //       courseCodeIdx++;
+  //     } 
+  //   }
+  // }
+
+  // Notify user of invalid URL
+  function addInvalidURLMessage() {
+    if (isScheduleOnPage) {
+      $(".scheduleLayout").remove();
+      isScheduleOnPage = false;
+    }
+    if (!isInvalidURLMessageOnPage) {
+      $(".schedule").append('<p class="invalidURL">Error: Invalid URL entered.<br />Please re-enter the URL.</p>');
+      isInvalidURLMessageOnPage = true;
+    }
+  }
+
+  // Remove invalid URL message if present and if valid URL
+  function removeInvalidURLMessage() {
+    if (isInvalidURLMessageOnPage) {
+      $(".invalidURL").remove();
+      isInvalidURLMessageOnPage = false;
+    }
+  }
+
+  // Create and display schedule
+  function createScheduleLayout() {
+    if (!isScheduleOnPage) {
+      var scheduleLayout = '<table class="scheduleLayout"> <thead><tr><th>Time</th> <th>Monday</th> <th>Tuesday</th> <th>Wednesday</th> <th>Thursday</th> <th>Friday</th></tr></thead>';
+      scheduleLayout += "<tbody>";
+  
+      for (var i = 8; i < 12; i++) {
+        scheduleLayout += '<tr><td>' + i + ':00AM</td><td></td><td></td><td></td><td></td><td></td></tr>';
+        scheduleLayout += '<tr><td>' + i + ':30AM</td><td></td><td></td><td></td><td></td><td></td></tr>';
+      }
+  
+      scheduleLayout += '<tr><td>12:00PM</td><td></td><td></td><td></td><td></td><td></td></tr>';
+      scheduleLayout += '<tr><td>12:30PM</td><td></td><td></td><td></td><td></td><td></td></tr>';
+  
+      for (var i = 1; i < 12; i++) {
+        scheduleLayout += '<tr><td>' + i + ':00PM</td><td></td><td></td><td></td><td></td><td></td></tr>';
+        scheduleLayout += '<tr><td>' + i + ':30PM</td><td></td><td></td><td></td><td></td><td></td></tr>';
+      }
+      
+      scheduleLayout += "</tbody>";
+      scheduleLayout += "</table>";
+  
+      $(".schedule").append(scheduleLayout);
+  
+      isScheduleOnPage = true;
+    }
+  }
 }
 
 $(document).ready(main);
--- php/getURLInfo.php
+++ php/getURLInfo.php
@@ -1,12 +1,49 @@
 <?php
-    $scheduleOfClassesURL = $_GET["scheduleOfClassesURL"];
+  $scheduleOfClassesURL = $_GET["scheduleOfClassesURL"];
 
-    $handle = curl_init();
-    curl_setopt($handle, CURLOPT_URL, $scheduleOfClassesURL);
-    curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
-    $html = curl_exec($handle);
+  $handle = curl_init();
+  curl_setopt($handle, CURLOPT_URL, $scheduleOfClassesURL);
+  curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+  $html = curl_exec($handle);
 
-    echo json_encode($html);
+  // Split array by newline
+  $lines = explode("\n", $html);
+  
+  // Regular expression for time and day of courses
+  $dayTimeRegex = "/((2[0-3]|[01][0-9]):([0-5][0-9])-(2[0-3]|[01][0-9]):([0-5][0-9]))([MTWhF]{0,6})/";
 
-    curl_close($handle);
+  $currCourseCodeIdx = -1;
+  $data = array(
+    array(
+      "courseCode" => array(),
+      "time" => array(),
+      "startTimeHour" => array(),
+      "startTimeMinute" => array(),
+      "endTimeHour" => array(),
+      "endTimeMinute" => array(),
+      "days" => array()
+    )
+  );
+
+  // Find course codes and corresponding time and day of courses
+  for ($line = 0; $line < count($lines); $line++){
+    if (!(strcmp($lines[$line], '<TR><TD ALIGN="center">BME     </TD>'))) {
+        $currCourseCodeIdx++;
+        $dayTimeIdx = 0;
+        $data[$currCourseCodeIdx]["courseCode"] = strip_tags($lines[$line + 1]);
+    } else if (preg_match($dayTimeRegex, strip_tags($lines[$line]), $matches)) {
+      $data[$currCourseCodeIdx]["time"][$dayTimeIdx] = $matches[1];
+      $data[$currCourseCodeIdx]["startTimeHour"][$dayTimeIdx] = $matches[2];
+      $data[$currCourseCodeIdx]["startTimeMinute"][$dayTimeIdx] = $matches[3];
+      $data[$currCourseCodeIdx]["endTimeHour"][$dayTimeIdx] = $matches[4];
+      $data[$currCourseCodeIdx]["endTimeMinute"][$dayTimeIdx] = $matches[5];
+      $data[$currCourseCodeIdx]["days"][$dayTimeIdx] = $matches[count($matches) - 1];
+
+      $dayTimeIdx++;
+    } 
+  }
+
+  echo json_encode($data);
+
+  curl_close($handle);
 ?>
